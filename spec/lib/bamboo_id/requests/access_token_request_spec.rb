8619e8bd0468b7fea4538b6add515696a1874c2f
Jon Evans <joncodes+github@gmail.com>
2020-03-30 15:59:01 -0600

    Handle an invalid response from Bamboo
    If Bamboo blows up and returns a string instead of JSON,
    let's handle it more gracefully.

--- spec/lib/bamboo_id/requests/access_token_request_spec.rb
+++ spec/lib/bamboo_id/requests/access_token_request_spec.rb
@@ -54,6 +54,24 @@ module BambooId
           end
         end
       end
+
+      context 'when bamboo blows up and returns an error page instead of a hash' do
+        let(:response_body) do
+          'oh no i blew up and this is a string'
+        end
+
+        describe '#successful?' do
+          it 'returns false' do
+            expect(request).to_not be_successful
+          end
+        end
+
+        describe '#access_token' do
+          it 'returns nil' do
+            expect(request.access_token).to be nil
+          end
+        end
+      end
     end
   end
 end
